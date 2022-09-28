832b1bbd2e44f56d1ddfdbca6ab69e8a0f397c7a
Song <misosjm@naver.com>
2022-09-28 23:42:39 +0900

    test

--- index.js
+++ index.js
@@ -27,56 +27,56 @@ app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
 app.set('views', path.join(__dirname, 'views'))
 
-sequelize
-    .sync({ force: false })
-    .then(() => {
-        console.log('데이터베이스 연결 성공')
-    })
-    .catch((err) => {
-        console.error(err)
-    })
-passportConfig()
+// sequelize
+//     .sync({ force: false })
+//     .then(() => {
+//         console.log('데이터베이스 연결 성공')
+//     })
+//     .catch((err) => {
+//         console.error(err)
+//     })
+// passportConfig()
 
-app.use(helmet())
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(cors(corsConfig))
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser(process.env.COOKIE_SECRET))
-app.use(
-    expressSession({
-        resave: true,
-        saveUninitialized: false,
-        secret: process.env.COOKIE_SECRET,
-        store: sessionStore,
-        cookie: {
-            httpOnly: true,
-            secure: false,
-        },
-        name: 'session: planner',
-    })
-)
-app.use(passport.initialize())
-app.use(passport.session())
+// app.use(helmet())
+// app.use(morgan('dev'))
+// app.use(express.json())
+// app.use(cors(corsConfig))
+// app.use(express.urlencoded({ extended: false }))
+// app.use(cookieParser(process.env.COOKIE_SECRET))
+// app.use(
+//     expressSession({
+//         resave: true,
+//         saveUninitialized: false,
+//         secret: process.env.COOKIE_SECRET,
+//         store: sessionStore,
+//         cookie: {
+//             httpOnly: true,
+//             secure: false,
+//         },
+//         name: 'session: planner',
+//     })
+// )
+// app.use(passport.initialize())
+// app.use(passport.session())
 
-app.use('/', indexRouter)
-app.use('/signup', signUpRouter)
-app.use('/login', loginRouter)
-app.use('/addPlans', addPlansRouter)
-app.use('/deleteId', deleteIdRouter)
+// app.use('/', indexRouter)
+// app.use('/signup', signUpRouter)
+// app.use('/login', loginRouter)
+// app.use('/addPlans', addPlansRouter)
+// app.use('/deleteId', deleteIdRouter)
 
-app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`)
-    error.status = 404
-    next(error)
-})
+// app.use((req, res, next) => {
+//     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`)
+//     error.status = 404
+//     next(error)
+// })
 
-app.use((err, req, res, next) => {
-    res.locals.message = err.message
-    res.locals.error = process.env.ENV !== 'production' ? err : {}
-    res.status(err.status || 500)
-    res.render('error')
-})
+// app.use((err, req, res, next) => {
+//     res.locals.message = err.message
+//     res.locals.error = process.env.ENV !== 'production' ? err : {}
+//     res.status(err.status || 500)
+//     res.render('error')
+// })
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기중')
